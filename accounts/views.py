6b682af43e5a6ff17458f1340b948aad7d82493f
ajeesh <ajeesh@reizend.ai>
2025-07-01 14:52:17 +0530

    initial commit

---
+++ accounts/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
+from django.http import HttpResponse
+
+def login_view(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return HttpResponse("Login successful!")
+        else:
+            return HttpResponse("Invalid credentials")
+    return render(request, 'login.html')
